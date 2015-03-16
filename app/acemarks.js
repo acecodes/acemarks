a349ddf7afe8739b7b32d97d82149b2d0e64ae43
Ace <ace.github@gmail.com>
2015-03-16 16:00:36 -0700

    Reorganized, added rudimentary bookmark features and used temporary fix for front page bug

--- app/acemarks.js
+++ app/acemarks.js
@@ -2,15 +2,86 @@ angular.module('AceMarks', []).controller(
     'PrimaryCtrl', function($scope) {
         $scope.testing = "Testing, 123";
 
-        // Sample data, to be replaced after implementing backend
-        $scope.categories = [
-            {"id": 0, "name": "Aviation"},
-            {"id": 1, "name": "Reading"},
-            {"id": 3, "name": "Nutrition"}
-        ];
+        $scope.user = "John Doe";
 
-        $scope.bookmarks = [
-            {"id": 0, "title":"Naval aviation", "url":"http://www.navy.com", "category":"Aviation"},
-            {"id": 0, "title":"Goodreads", "url":"http://www.goodreads.com", "category":"Reading"}
-        ];
+        // Sample data, to be replaced after implementing backend
+        $scope.categories = [{
+            "id": 0,
+            "name": "Aviation"
+        }, {
+            "id": 1,
+            "name": "Reading"
+        }, {
+            "id": 2,
+            "name": "Nutrition"
+        }];
+
+        $scope.bookmarks = [{
+            "id": 0,
+            "title": "Naval aviation",
+            "url": "http://www.navy.com",
+            "category": "Aviation"
+        }, {
+            "id": 1,
+            "title": "Goodreads",
+            "url": "http://www.goodreads.com",
+            "category": "Reading"
+        }];
+
+        // Categories
+
+        $scope.currentCategory = null;
+
+        function setCurrentCategory(category) {
+            $scope.currentCategory = category;
+        }
+
+        function isCurrentCategory(category) {
+            return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+        }
+
+        // Creating/editing states
+
+        $scope.isCreating = false;
+        $scope.isEditing = false;
+
+        function startCreating() {
+            $scope.isCreating = true;
+            $scope.isEditing = false;
+        }
+
+        function cancelCreating() {
+            $scope.isCreating = false;
+        }
+
+        function startEditing() {
+            $scope.isCreating = false;
+            $scope.isEditing = true;
+        }
+
+        function cancelEditing() {
+            $scope.isEditing = false;
+        }
+
+        function shouldShowCreating() {
+            return $scope.currentCategory && !$scope.isEditing;
+        }
+
+        function shouldShowEditing() {
+            return $scope.isEditing && !$scope.isCreating;
+        }
+
+
+        // Make functions public (available to views)
+
+        $scope.setCurrentCategory = setCurrentCategory;
+        $scope.isCurrentCategory = isCurrentCategory;
+
+        $scope.startCreating = startCreating;
+        $scope.cancelCreating = cancelCreating;
+        $scope.startEditing = startEditing;
+        $scope.cancelEditing = cancelEditing;
+        $scope.shouldShowCreating = shouldShowCreating;
+        $scope.shouldShowEditing = shouldShowEditing;
+
     });
