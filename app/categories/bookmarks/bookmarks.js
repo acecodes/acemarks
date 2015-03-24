063b7733653c249503490360d9fe6c8e58ed4883
Ace <ace.github@gmail.com>
2015-03-23 20:47:23 -0700

    More routing behavior, this time for categories

--- app/categories/bookmarks/bookmarks.js
+++ app/categories/bookmarks/bookmarks.js
@@ -3,4 +3,19 @@ angular.module('categories.bookmarks', [
     'categories.bookmarks.edit',
     'AceMarks.models.categories',
     'AceMarks.models.bookmarks'
-    ]);
+])
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('acemarks.categories.bookmarks', {
+                url: 'categories/:category',
+                views: {
+                    'bookmarks@': {
+                        templateUrl: 'app/categories/bookmarks/bookmarks.html',
+                        controller: 'BookmarksCtrl'
+                    }
+                }
+            });
+    })
+    .controller('BookmarksCtrl', function($scope, $stateParams) {
+        $scope.currentCategoryName = $stateParams.category;
+    });
